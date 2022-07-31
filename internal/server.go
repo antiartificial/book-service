059edeb7dc0b46587d684fc34ba6e9cee5490581
Aaron Barton <antiartificial@gmail.com>
2022-07-30 21:50:12 -0700

    Initial commit.

---
+++ internal/server.go
@@ -0,0 +1,20 @@
+package internal
+
+import (
+	api "github.com/antiartificial/bookservice/api/v1"
+	"google.golang.org/grpc"
+)
+
+type grpcServer struct {
+	BookRepository BookRepository
+	api.UnimplementedBookServiceServer
+}
+
+func NewRPCServer(repository BookRepository) *grpc.Server {
+	srv := grpcServer{
+		BookRepository: repository,
+	}
+	gsrv := grpc.NewServer()
+	api.RegisterBookServiceServer(gsrv, &srv)
+	return gsrv
+}
